849a0ad2c3bdcc2bfc077148f7e650ca131b1eb2
Scott Rock <scottrockbiz@gmail.com>
2018-08-10 09:51:06 -0700

    all pages and routes added for assignment

--- app.js
+++ app.js
@@ -14,7 +14,28 @@ var server = http.createServer(function(request, response) {
       response.write(contents);
       response.end();
     });
-  } else {
+  } else if ( request.url === '/stylesheets/style.css' ) {
+      // style sheet
+      fs.readFile('stylesheets/style.css', 'utf8', function(errors, contents) {
+        response.writeHead(200, {'Content-Type': 'text/css'});
+        response.write(contents);
+        response.end();
+      });
+    } else if ( request.url === '/ninjas' ) {
+        // style sheet
+        fs.readFile('ninjas.html', 'utf8', function(errors, contents) {
+          response.writeHead(200, {'Content-Type': 'text/html'});
+          response.write(contents);
+          response.end();
+        });
+      } else if ( request.url === '/dojos/new' ) {
+          // style sheet
+          fs.readFile('dojos.html', 'utf8', function(errors, contents) {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+          });
+        } else {
     // no matching route
     response.writeHead(404, 'File not found!!!')
   }
